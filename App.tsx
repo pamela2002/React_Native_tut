7ef25edd0c1f027b896ed1585198e53c4cd8ebbf
pamela2002 <sroychoudhury65@gmail.com>
2024-04-11 19:11:44 +0530

    Today I've coverd the topis - JSX, components, events, states, props

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React,{useState} from 'react';
+import {Text, View, Button} from 'react-native';
 
 const App = () => {
+  const [name, setName] = useState("Pamela");
+  const age = 22;
   return (
     <View>
-      <Text>Hello React Native</Text>
+      <Text style={{ fontSize: 30 }}>Hello {name}</Text>
+      <Button title='Press Here' onPress={() => { setName("Pam") }} color={"green"}></Button>
+      <UserData age={age} />
     </View>
   );
+}
+
+const UserData = (props) => {
+  return (
+    <View>
+      <Text style={{fontSize:20}}>My age is {props.age}</Text>
+    </View>
+  )
 }
 
 export default App;

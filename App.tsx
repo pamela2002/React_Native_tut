ec1a0a47ce0c585e55c192214886299e22f1de51
pamela2002 <sroychoudhury65@gmail.com>
2024-05-08 21:07:37 +0530

    Today I've covered the topic - Responsive UI in react-native

--- App.tsx
+++ App.tsx
@@ -1,37 +1,51 @@
-
-
-import React,  { useEffect, useState }  from "react";
-import { Text, View, Button } from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 const App = () => {
-  const [show, setShow] = useState(true);
   return (
-    <View>
-      <Text style={{ fontSize: 30 }}>userEffect for Unmount Component</Text>
-      <Button title="Toggle" onPress={() => setShow(!show)} /> 
-     {
-       show ? <Student/> : null
-     }
-    
-   </View>
-  )
-}
-
-const Student = () => {
-  const timer = setInterval(() => {
-      console.warn("Timer called")
-    }, 2000);
-  useEffect(() => {
-    return () => clearInterval(timer);
-  });
-  return (
-   <View>
-       <Text style={{fontSize:30, color:"red"}}>Student</Text>
-     </View>
-   )
- }
-
- export default App;
+    <View style={styles.main}>
+      <View style={styles.box1}>
+        <View style={styles.innerBox1}></View>
+        <View style={styles.innerBox2}></View>
+        <View style={styles.innerBox3}></View>
+      </View>
+      <View style={styles.box2}></View>
+      <View style={styles.box3}></View>
+    </View>
+  );
+};
+export default App;
 
 
-
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+  },
+  box1: {
+    flex: 2,
+    backgroundColor: 'red',
+    flexDirection: 'row',
+  },
+  box2: {
+    flex: 1,
+    backgroundColor: 'green',
+  },
+  box3: {
+    flex: 1,
+    backgroundColor: 'blue',
+  },
+  innerBox1: {
+    flex: 1,
+    backgroundColor: 'skyblue',
+    margin: 10,
+  },
+  innerBox2: {
+    flex: 1,
+    backgroundColor: 'pink',
+    margin: 10,
+  },
+  innerBox3: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    margin: 10,
+  },
+});

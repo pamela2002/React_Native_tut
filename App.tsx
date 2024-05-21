e6c8f188a8166d7c69abe1859bde7c931acadb38
pamela2002 <sroychoudhury65@gmail.com>
2024-05-21 23:59:39 +0530

    Check Platform  in react-native, Install NPM package in react-native done

--- App.tsx
+++ App.tsx
@@ -1,63 +1,41 @@
-// import {View, StyleSheet, Text, Pressable} from 'react-native';
+// import { View, Text, StyleSheet, Platform } from "react-native";
+
 
 // const App = () => {
-//   return (
-//     <View style={styles.main}>
-//           <Pressable onPress={() => { console.warn("normal on press") }}
-//               onLongPress={() => { console.warn("normal on press") }}
-//               onPressIn={() => { console.warn("press in") }}
-//               onPressOut={() => { console.warn("press out") }}>
-//         <Text style={styles.pressableBtn}>Pressable</Text>
-//       </Pressable>
-//     </View>
-//   );
-// };
+//     return (
+//         <View>
+//             <Text style={{ fontSize: 20 }}>Platform: {Platform.OS}</Text>
+//             {
+//                 Platform.OS === "android" ?
+//                     <View style={{ backgroundColor: "green", height: 100, width: 100 }}></View> :
+//                     <View style={{ backgroundColor: "red", height: 100, width: 100 }}></View>
+//             }
+//             <Text style={styles.text}>Hello</Text>
+//             <Text style={{fontSize:20}}>{JSON.stringify(Platform)}</Text>
+//         </View>
+//     )
+// }
+
+
 
 // const styles = StyleSheet.create({
-//   main: {
-//     flex: 1,
-//     justifyContent: 'center',
-//   },
-//   pressableBtn: {
-//     backgroundColor: 'blue',
-//     color: '#fff',
-//       padding: 10,
-//       margin: 10,
-//       borderRadius: 10,
-//       fontSize: 20,
-//       textAlign: 'center',
-//       shadowColor: '#000',
-//     elevation: 5,
-//   },
-// });
+//     text: {
+//         color: Platform.OS === "android" ? "orange" : "blue",
+//         fontSize:20,
+//     }
+// })
 
 // export default App;
 
-
-import { useState } from "react";
-import { View, StatusBar, StyleSheet, Button } from "react-native";
+import { View, Text } from "react-native";
+import WebView from "react-native-webview";
 
 const App = () => {
-    const [hide, setHide] = useState(false);
-    const [barStyle, setBarStyle] = useState("default");
     return (
-        <View style={styles.container}>
-            <StatusBar backgroundColor="orange"
-                barStyle={barStyle}
-                hidden={hide} />
-            <Button title="Toggle Status Bar" onPress={()=>{setHide(!hide)}}/>
-            <Button title="Update Style" onPress={()=>{setBarStyle("dark-content")}}/>
-        </View>
+       
+            <WebView source={{uri: "https://reactnative.dev/"}}/>
+        
     )
 }
-
-
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-    }
-});
 
 export default App;

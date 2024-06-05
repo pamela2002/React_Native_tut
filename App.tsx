d030957bad444f63d7d772d5c2352226bf03fd74
pamela2002 <sroychoudhury65@gmail.com>
2024-06-05 23:45:09 +0530

    Today I've learnt the topic: Stack Navigation Button in react-native

--- App.tsx
+++ App.tsx
@@ -4,6 +4,9 @@ import {Button, Text, View} from 'react-native';
 
 const Stack = createNativeStackNavigator();
 const App = () => {
+  const btnAction = () => {
+    console.warn('btn pressed');
+  };
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -17,12 +20,24 @@ const App = () => {
             fontSize: 25,
           },
         }}>
-        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Login" component={Login}
+          options={{
+            headerTitle: () => <Button onPress={btnAction} title='Left' />,
+            headerRight:() =><Headers/>
+        }}/>
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
+const Headers = () => {
+    return (
+      <Button title='btn'/>
+    )
+  }
+
+
 
 const Home = () => {
   return (
@@ -32,16 +47,16 @@ const Home = () => {
   );
 };
 
-const Login = props => {
+const Login = (props) => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 30, alignContent: 'center'}}>Login Page</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 30, alignContent: 'center' }}>Login Page</Text>
       <Button
         title="Go To Home Page"
         onPress={() => props.navigation.navigate('Home')}
       />
     </View>
   );
-};
+}
 
 export default App;
